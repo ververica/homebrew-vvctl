6dcc8a87a67c231ed005ebafceb13f5445ebb03c
Hector Sanchez Pajares <34852+hecspc@users.noreply.github.com>
2025-06-27 13:12:26 +0200

    Merge pull request #1 from ververica/update-vvctl.rb-1751022697
    vvctl 2025.6.27

--- vvctl.rb
+++ vvctl.rb
@@ -1,9 +1,9 @@
 class Vvctl < Formula
-  version "2025.6.26"
+  version "2025.6.27"
   desc "CLI application for Ververica Platform"
   homepage "https://app.ververica.cloud/"
-  url "https://github.com/ververica/vvctl/releases/download/2025.6.26/vvctl-2025.6.26-aarch64-apple-darwin.tar.gz"
-  sha256 "a04b6872d33b5a8e502a55f1774d211929fd5142ad9b1683ecd2f545e3fc3ef7"
+  url "https://github.com/ververica/homebrew-vvctl/releases/download/2025.6.27/vvctl-2025.6.27-aarch64-apple-darwin.tar.gz"
+  sha256 "108fb0810455a6d31c67ddfb431761bf664a1f13c5b5ce5cd9f1b8c736c1ce0f"
   license "Your-License"
 
   def install
