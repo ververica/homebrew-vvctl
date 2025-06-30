ad6a8362526b9bb3780b60e8a09e90ba1fbeafa0
Hector Sanchez Pajares <34852+hecspc@users.noreply.github.com>
2025-06-30 12:43:02 +0200

    vvctl 2025.6.29

--- vvctl.rb
+++ vvctl.rb
@@ -1,9 +1,9 @@
 class Vvctl < Formula
-  version "2025.6.27"
+  version "2025.6.29"
   desc "CLI application for Ververica Platform"
   homepage "https://app.ververica.cloud/"
-  url "https://github.com/ververica/homebrew-vvctl/releases/download/2025.6.27/vvctl-2025.6.27-aarch64-apple-darwin.tar.gz"
-  sha256 "108fb0810455a6d31c67ddfb431761bf664a1f13c5b5ce5cd9f1b8c736c1ce0f"
+  url "https://github.com/ververica/homebrew-vvctl/releases/download/2025.6.29/vvctl-2025.6.29-aarch64-apple-darwin.tar.gz"
+  sha256 "5031ef0cfc016b9e338e60edd149bd549e7d8ad16974198a9fea4462c63d5325"
   license "Your-License"
 
   def install
