4fc819646fbc8b04c12df6be373b54d6708ca259
Hector Sanchez Pajares <34852+hecspc@users.noreply.github.com>
2026-02-10 11:43:40 +0100

    chore: bump version

--- vvctl.rb
+++ vvctl.rb
@@ -1,16 +1,16 @@
 class Vvctl < Formula
-  version "2025.7.9"
+  version "2026.2.1"
   desc "CLI application for Ververica Platform"
   homepage "https://app.ververica.cloud/"
-  license "® Ververica GmbH 2025"
+  license "® Ververica GmbH 2026"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ververica/homebrew-vvctl/releases/download/2025.7.9/vvctl-2025.7.9-aarch64-apple-darwin.tar.gz"
-      sha256 "c618d2890373765bc9ae227b06ff15c3a8be3fd970b877d235e506b50287c5ad"
+      url "https://github.com/ververica/homebrew-vvctl/releases/download/2025.7.9/vvctl-2026.2.1-aarch64-apple-darwin.tar.gz"
+      sha256 "337f86e21817c573c2dcdf287404675fb0b1c2f1b294c3318337c9c49e523d75"
     else
-      url "https://github.com/ververica/homebrew-vvctl/releases/download/2025.7.9/vvctl-2025.7.9-x86_64-apple-darwin.tar.gz"
-      sha256 "ed487cda83857c753906a9ca344091aafd1981e09fba1018a9ce2a2659560116"
+      url "https://github.com/ververica/homebrew-vvctl/releases/download/2025.7.9/vvctl-2026.2.1-x86_64-apple-darwin.tar.gz"
+      sha256 "247bad75b0609e199b6fae8656dc9840ea198f05bf92597aea8520ce31de0129"
     end
   end
 
